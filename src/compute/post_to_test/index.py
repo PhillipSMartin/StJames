a7c28f0e7d9b2c82079ed927dcd6c5def712cab7
Phillip Martin <sarabandoid@gmail.com>
2024-09-30 12:18:39 -0400

    Improved error handling in post_to_test

--- src/compute/post_to_test/index.py
+++ src/compute/post_to_test/index.py
@@ -6,79 +6,90 @@ import os
 from botocore.exceptions import ClientError
 
 website = 'test'
-error = None
 
 sns = boto3.client('sns')
 topic_arn = os.environ['TOPIC_ARN']  
 
 def handler(event, context):
-    global error
-
+    error_message = None 
     events_posted = 0
     events_failed = 0
 
     try:
         for record in event["Records"]:
 
+            # Retrieve info about event to post
             item = json.loads(record["Sns"]["Message"])
             print("Request:", json.dumps(item))
 
-            if not update_status(item, 'posting'):
+            # Set status to 'posting' to prevent duplicate posts
+            # Currrent status should be 'post' - returns False if it isn't
+            success, error_message = update_status(item, 'posting')
+            if not success:
                 events_failed += 1
-                post_to_sns(False, item)
+                post_to_sns(False, item, error_message)
                 continue
 
-            if post_to_website(item):
+            # Post to website
+            success, error_message = post_to_website(item)
+            if success:
                 events_posted += 1
                 print(f"Posted: { item['title'] }")
+
+                # Set status to 'posted'
                 update_status(item, 'posted')
                 post_to_sns(True, item)
 
             else:
-                update_status(item, 'failed')
-                post_to_sns(False, item)
+                events_failed += 1
+                print(f"Failed to post { item['title'] }: { error_message }")
+
+                # Set status back to 'post' so we can try again after fixing the issue
+                update_status(item, 'post')
+                post_to_sns(False, item, error_message)
 
         body = f"Posted {events_posted} events, failed to post {events_failed} events"
         print(body)
 
         return {
             'statusCode': 200,
-            'body': json.dumps({ 'message': body })
+            'body': json.dumps({ 'error_message': body })
         }
                     
     except json.JSONDecodeError as e:
-        error = f"Error decoding JSON: {e}"
-        print(error)
-        post_to_sns(False)       
+        error_message = f"Error decoding JSON: {e}"
+        print(error_message)
+        post_to_sns(False, None, error_message)
         return {
             'statusCode': 400,
-            'body': json.dumps({ 'message': 'Invalid JSON in event' })
+            'body': json.dumps({ 'error_message': 'Invalid JSON in event' })
         }
     
     except Exception as e:
-        error = f"Unexpected error: {e}"
-        print(error)
-        post_to_sns(False)
+        error_message = f"Unexpected error: {e}"
+        print(error_message)
+        post_to_sns(False, None, error_message)
         return {
             'statusCode': 500,
-            'body': json.dumps({ 'message': 'Internal error' })
+            'body': json.dumps({ 'error_message': 'Internal error' })
         }
     
-def post_to_sns(success, item=None): 
+def post_to_sns(success, item, error_message=None): 
     try:      
         sns.publish(
             TopicArn=topic_arn,
-            Message=error or 'No errors',
+            Message=error_message or 'No errors',
             Subject=f"Post to {website} {'succeeded' if success else 'failed'}: { item['title'] if item is not None else '' }"
         )
     except Exception as e:
         print(f"Failed to post to SNS: {e}")
 
 def update_status(item, new_status):
-    return True       
+    print(f"Updating status of {item['title']} to {new_status}")
+    return False, "Testing error message handling"       
  
 def post_to_website(item):    
-    print (f'Posting {item["title"]}')
-    return True        
+    print (f'Posting {item["title"]} to {website}')
+    return True, None        
 
 
